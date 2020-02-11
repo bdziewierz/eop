c748677bb58217e35cc568ffbfc4641bab5f9cb0
Boguslaw Dziewierz <boguslaw.dziewierz@dazn.com>
2020-02-11 14:44:30 +0000

    Uped versions of dependencies and modified the code to support dealing 'hands'

--- index.js
+++ index.js
@@ -30,12 +30,15 @@ app.get('/', function (req, res) {
   var s = req.query.s;
   var c = req.query.c;
   var m = req.query.m;
+  var p = req.query.p;
+  var h = req.query.h;
 
   // Get the key from the env
   var key = process.env.KEY
   var hmac;
+  var num_cards = 74;
 
-  var seed, card, mac, newmac;
+  var seed, card, mac, newmac, hand, participants;
 
   if ((typeof s == 'undefined') && (typeof p == 'undefined') && (typeof m == 'undefined')) {
     console.log("New deck");
@@ -45,7 +48,9 @@ app.get('/', function (req, res) {
     console.log("Setting card number to: "+card);
     mac = macState(seed, card, key);
     console.log("HMAC created: "+mac);
-    var url = req.protocol+"://"+req.get('Host')+"/?s="+seed+"&c="+card+"&m="+mac;
+    participants = 3;
+    hand = 8;
+    var url = req.protocol+"://"+req.get('Host')+"/?s="+seed+"&c="+card+"&m="+mac+"&p="+participants+"&h="+hand;
     console.log("Card URL is: "+url);
     res.redirect(url);
   } else if (s == "") {
@@ -56,11 +61,20 @@ app.get('/', function (req, res) {
     res.render('error', {
       error: 'card number (p) missing'
     });
+  } else if (p == "") {
+    res.render('error', {
+      error: 'participants (h) missing'
+    });
   } else if (m == "") {
     res.render('error', {
       error: 'mac token (m) missing'
     });
+  } else if (h == "") {
+    res.render('error', {
+      error: 'hand (h) missing'
+    });
   } else {
+
     console.log("Existing deck")
     seed = s
     console.log("Seed given: "+seed)
@@ -68,6 +82,11 @@ app.get('/', function (req, res) {
     console.log("Card given: "+card)
     mac = m
     console.log("HMAC given: "+mac)
+    participants = p;
+    console.log("Participants given: "+participants)
+    //hand = Math.floor(num_cards / participants);
+    hand = h;
+    console.log("HAND given: "+hand)
 
     newmac = macState(seed, card, key);
     console.log("New HMAC: "+newmac)
@@ -77,41 +96,50 @@ app.get('/', function (req, res) {
         error: 'incorrect mac'
       });
     } else {
-      var num_cards = 74;
-      var currenturl = req.protocol+"://"+req.get('Host')+"/?s="+seed+"&c="+card+"&m="+mac;
+      var currenturl = req.protocol+"://"+req.get('Host')+"/?s="+seed+"&c="+card+"&m="+mac+"&p="+participants+"&h="+hand;
       var url, prevurl, prevcard
 
-      console.log("Creating previous url")
-      if (Number(card) > 1) {
-        prevcard = Number(card) - 1
-        hmac = crypto.createHmac('sha1', key)
-        prevmac = macState(seed, prevcard, key);
-        prevurl = "/?s="+seed+"&c="+prevcard+"&m="+prevmac;
-      }
 
-      console.log("Creating cards array");
-      var cards = Array(num_cards).join(0).split(0).map(Number.call, Number);
-      var rand = gen.create(seed);
-      var shuffled_cards = rand_shuffle(rand, cards);
-      var cardIndex = Number(card) - 1;
-      var cardValue = shuffled_cards[cardIndex];
-      console.log("Current card value: "+cardValue);
+      var cardValues = []
+      var newCard = card;
+      for (var i=1; i<=hand; ++i)  {
 
-      if (Number(card) < num_cards) {
-        console.log("Creating HMAC for new card")
-        var newCard = Number(card) + 1;
-        console.log("New card number: "+newCard);
-        console.log("Seed is: "+seed);
-        mac = macState(seed, newCard, key);
-        console.log("New HMAC is: "+mac);
+        newCard++;
 
-        url = "/?s="+seed+"&c="+newCard+"&m="+mac;
-        url = req.protocol+"://"+req.get('Host')+"/?s="+seed+"&c="+newCard+"&m="+mac;
-        console.log("Next card URL is: "+url);
-        console.log("Last card: "+macState(seed, num_cards, key));
+        console.log("Creating previous url")
+        if (Number(card) > 1) {
+          prevcard = Number(card) - 1
+          hmac = crypto.createHmac('sha1', key)
+          prevmac = macState(seed, prevcard, key);
+          prevurl = "/?s="+seed+"&c="+prevcard+"&m="+prevmac;
+        }
+
+        // Generate set number of cards, please
+        console.log("Creating cards array");
+        var cards = Array(num_cards).join(0).split(0).map(Number.call, Number);
+        var rand = gen.create(seed);
+        var shuffled_cards = rand_shuffle(rand, cards);
+        var cardIndex = newCard - 1;
+        var cardValue = shuffled_cards[cardIndex];
+        cardValues.push(cardValue);
+        console.log("Current card value: "+cardValue);
+  
+        if (Number(newCard) < num_cards) {
+          console.log("Creating HMAC for new card")
+          console.log("New card number: "+newCard);
+          console.log("Seed is: "+seed);
+          mac = macState(seed, newCard, key);
+          console.log("New HMAC is: "+mac);
+  
+          url = "/?s="+seed+"&c="+newCard+"&m="+mac;
+          url = req.protocol+"://"+req.get('Host')+"/?s="+seed+"&c="+newCard+"&m="+mac+"&p="+participants+"&h="+hand;
+          console.log("Next card URL is: "+url);
+          console.log("Last card: "+macState(seed, num_cards, key));
+        }
       }
 
       res.render('index', { 
+        cardValues: cardValues,
         cardValue: cardValue,
         seed: seed,
         card: card,
